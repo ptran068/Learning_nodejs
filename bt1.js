8cc53717efd6d71c79581cc9d2e38f000b0a2204
ptran068 <ptran068@gmail.com>
2018-12-09 09:31:05 +0700

    a

---
+++ bt1.js
@@ -0,0 +1,63 @@
+const listStudents = [];
+
+class Student {
+
+    static addStudent(name, age, isFemale) {
+        let infoStudent = {
+            name : name,
+            age : age,
+            isFemale : isFemale
+        };
+         listStudents.push(infoStudent);
+          return listStudents;
+    }
+
+    static getAllStudent() {
+        if(listStudents.length <= 0){
+            console.log('we have no student');
+        }
+        else {
+            for (let i = 0; i < listStudents.length; i++) {
+                console.log(listStudents[i]);
+            }
+        }
+         return listStudents;
+    }
+
+    //lấy sinh viên theo vị trí trong mảng
+    static getStudentByIndex(index) {
+        for (let i = 0; i < listStudents.length; i++) {
+            if(listStudents[i] === listStudents[index]) {
+                console.log(listStudents[i]);
+            }          
+        }
+         return listStudents;
+    }
+
+    //viết hàm trả về vị trí của sinh viên theo mảng theo tên.
+    static getIndexOfStudentByName(name) {
+        for (let i = 0; i < listStudents.length; i++) {
+            if(listStudents[i].name === name)
+                console.log('Position of ' + name + ' ' + 'is ' + i);
+        }
+    }
+
+    //remove student in arrays by name
+    static removeStudentByName(name) {
+        for (let i = 0; i < listStudents.length; i++) {
+            if(listStudents[i].name === name) {
+                return listStudents.splice(i,1);
+            }
+        }
+    }
+}
+
+Student.addStudent('phong',20,false);
+Student.addStudent('nhan',20,false);
+Student.addStudent('tuan',20,false);
+Student.addStudent('son',20,false);
+Student.getAllStudent();
+Student.getStudentByIndex(2);
+Student.getIndexOfStudentByName('son');
+Student.removeStudentByName('phong');
+Student.getAllStudent();
---
+++ loop.js
@@ -0,0 +1,36 @@
+
+// for (let i = 0; i < arr.length; i++) {};
+// for (const item of arr) {};
+// const names  = ['C','D'];
+// names.push('A');
+// names.push('B');
+// const topName = names.pop();
+// console.log(topName);
+
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i]);
+// };
+
+// for (const iteam of names) {
+//     console.log(iteam);
+// };
+
+//verb+noun
+// function getNameByIndex(index) {
+//     return names[index];
+// }
+
+// console.log(getNameByIndex(1));
+
+class Student {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    showInfor() {
+        console.log('student ' + this.name + ' ' + this.age);
+    }
+};
+
+const getInfo = new Student('phong',20);
+getInfo.showInfor();
